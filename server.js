b4562172f64570885568116112d85edac6da40b7
gourishankarprusty07 <100337318+gourishankarprusty07@users.noreply.github.com>
2022-09-10 23:39:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('josn-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
